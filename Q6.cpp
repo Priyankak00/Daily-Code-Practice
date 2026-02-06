fad564c247f3c4fddee60849903c16af53325beb
hka <151080996+Priyanka-Kri6a@users.noreply.github.com>
2026-02-06 16:52:13 +0530

    Q6 and Q7 of 100 questions by Priyanka

---
+++ Q6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x,y,z;
+    cin>>x>>y>>z;
+    cout<<"The greatest of the three numbers is: ";
+    if(x>y){
+        if(x>z)
+        cout<<x;
+        else
+        cout<<z;
+    }else
+    if(y>z)
+    cout<<y;
+    else
+    cout<<z;
+    return 0;
+}
